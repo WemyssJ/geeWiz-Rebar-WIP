d5df01b36f3d6a8916efea84c6455754efcaf163
Joe Wemyss <jwwemyss@gmail.com>
2025-05-29 20:48:47 +0100

    Add files via upload

---
+++ src/geeWiz/Forms/EiBreRebarUtils UI/FormRenumber.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Revit.DB;
+
+namespace NO.RebarUtils
+{
+    public partial class FormRenumber : System.Windows.Forms.Form
+    {
+        //constructor
+        public FormRenumber(Document doc,string[] partitions, Dictionary<string, string[]> rebarNumbers)
+        {
+            InitializeComponent();
+            this.doc = doc;
+            this.rebarNumbers = rebarNumbers;
+            comboPartition.Items.AddRange(partitions);
+            comboPartition.SelectedItem = 0;
+            comboRebarNumber.Items.AddRange(rebarNumbers[partitions.First()]);
+        }
+        
+        //Properties:
+        private Document doc { get; set; }
+        private Dictionary<string, string[]> rebarNumbers { get; set; }
+        public string partition { get; set; }
+        public int fromNumber { get; set; }
+        public int toNumber { get; set; }
+
+        //Events:
+        private void comboPartitions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try 
+            {
+                comboRebarNumber.Items.Clear();
+                comboRebarNumber.Items.AddRange(rebarNumbers[comboPartition.Text]);
+            }
+            catch (KeyNotFoundException)
+            {
+                comboRebarNumber.Items.Clear();
+            }
+        }
+
+        private void buttonChange_Clicked(object sender, EventArgs e)
+        {
+            partition = comboPartition.Text;
+            fromNumber = int.Parse(comboRebarNumber.Text);
+            toNumber = int.Parse(textNewNumber.Text);
+        }
+        private void buttonClose_Clicked(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
